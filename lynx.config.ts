76e4b93787e822f51b9b643bb124e9ec2700c2c5
Amirhossein Rezapanah <a.rezapanah@dotin.ir>
2025-04-07 11:15:29 +0330

    feat: initial commit

--- lynx.config.ts
+++ lynx.config.ts
@@ -6,7 +6,7 @@ import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
 export default defineConfig({
   server: {
     port: 3000,
-    host: "172.20.10.2",
+    host: "172.19.24.155",
   },
   plugins: [
     pluginQRCode({
