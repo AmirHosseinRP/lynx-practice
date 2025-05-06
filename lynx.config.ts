74c210b0739016ddc428ca5acff6b764ddcf5552
Amirhossein Rezapanah <a.rezapanah@dotin.ir>
2025-05-06 14:26:22 +0330

    feat: prettier file implemented

--- lynx.config.ts
+++ lynx.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from '@lynx-js/rspeedy'
+import { defineConfig } from "@lynx-js/rspeedy";
 
-import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin'
-import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
+import { pluginQRCode } from "@lynx-js/qrcode-rsbuild-plugin";
+import { pluginReactLynx } from "@lynx-js/react-rsbuild-plugin";
 
 export default defineConfig({
   server: {
     port: 3000,
-    host: "172.19.24.155",
+    host: "172.20.10.6",
   },
   plugins: [
     pluginQRCode({
       schema(url) {
-        // We use `?fullscreen=true` to open the page in LynxExplorer in full screen mode
-        return `${url}?fullscreen=true`
+        return `${url}?fullscreen=true`;
       },
     }),
-    pluginReactLynx(),
+    pluginReactLynx({
+      enableCSSInheritance: true,
+    }),
   ],
-})
+});
